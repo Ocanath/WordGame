d0fa42665ad1b474756ffb984dc2fd9cd49016f8
ocanath <ocanath@gmail.com>
2021-11-25 12:49:48 -0600

    progress, permutes all substrings

--- permute_def.py
+++ permute_def.py
@@ -1,21 +1,29 @@
 import enchant
+d = enchant.Dict("en_US")
 # Python program to print all permutations with
 # duplicates allowed
-  
+
 def toString(List):
-    return ''.join(List)
-  
+	return ''.join(List)
+
 # Function to print permutations of string
 # This function takes three parameters:
 # 1. String
 # 2. Starting index of the string
 # 3. Ending index of the string.
 def permute(a, l, r):
-    if l == r:
-        print(toString(a))
-    else:
-        for i in range(l, r + 1):
-            a[l], a[i] = a[i], a[l]
-            permute(a, l + 1, r)
-            a[l], a[i] = a[i], a[l] # backtrack
-  
+
+	if l == r:
+		n = len(a)
+		if (n <= 1):
+			return
+		str = toString(a)
+		
+		if(d.check(str) == True):
+			print(str)
+		permute(a[0:n-1],0,n-2)
+	else:
+		for i in range(l, r + 1):
+			a[l], a[i] = a[i], a[l]
+			permute(a, l + 1, r)
+			a[l], a[i] = a[i], a[l] # backtrack
